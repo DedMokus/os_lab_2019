07f28592adb8da9e33b6b33b66cd91f4e0dc6db4
DedMokus <pavllebd1998@mail.ru>
2024-09-07 12:44:36 +0300

    dfg

--- update.sh
+++ update.sh
@@ -1,5 +1,3 @@
-sudo apt -y update
-sudo apt -y upgrade
 sudo apt -y install man
 sudo apt -y install nano
 sudo apt -y install clang-format
@@ -7,3 +5,4 @@ sudo apt -y install iputils-ping
 sudo apt -y install gcc
 sudo apt -y install dos2unix
 sudo apt -y install libcunit1 libcunit1-doc libcunit1-dev
+sudo apt -y install go
