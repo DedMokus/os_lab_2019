6b0496cba1c4cb7d312a2a4506115ab08edd3b6d
DedMokus <pavllebd1998@mail.ru>
2024-09-20 16:12:30 +0300

    lab2

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "revert_string.h"
 
 void RevertString(char *str)
 {
-	// your code here
+	int first = 0;
+    int last = strlen(str) - 1;
+    char temp;
+
+    // Swap characters till first and last meet
+    while (first < last) {
+      
+        // Swap characters
+        temp = str[first];
+        str[first] = str[last];
+        str[last] = temp;
+
+        // Move pointers towards each other
+        first++;
+        last--;
+    }
 }
 
